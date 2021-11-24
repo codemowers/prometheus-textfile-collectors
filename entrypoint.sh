3e2dd33cd3ee75c0153486181640bafb4403cc1d
Lauri Võsandi <lauri@pinecrypt.com>
2021-11-24 15:26:58 +0200

    More fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 while true; do
-  /scripts/smartmon.sh > /run/node-exporter/smart.prom
-  /scripts/nvme_metrics.sh > /run/node-exporter/nvme.prom
-  /scripts/bridge.sh > /run/node-exporter/bridge.prom
-  /scripts/ipmitool > /run/node-exporter/impitool.prom
+  /scripts/smartmon.sh > /run/node-exporter/smart.prom.part
+  mv /run/node-exporter/smart.prom.part /run/node-exporter/smart.prom
+  /scripts/nvme_metrics.sh > /run/node-exporter/nvme.prom.part
+  mv /run/node-exporter/nvme.prom.part /run/node-exporter/nvme.prom
+  /scripts/bridge.sh > /run/node-exporter/bridge.prom.part
+  mv /run/node-exporter/bridge.prom.part /run/node-exporter/bridge.prom
+  ipmitool sensor | /scripts/ipmitool > /run/node-exporter/impitool.prom.part
+  mv /run/node-exporter/impitool.prom.part /run/node-exporter/impitool.prom
   sleep 15
 done
